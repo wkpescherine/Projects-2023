0cd1e72acd72c70aa9cea8b1cdbbf06f838f3018
wkpescherine <wkpescherine@gmail.com>
2022-11-24 21:43:08 -0500

    ESL 0.1 - Initial setup and preparation of basic project

---
+++ app/src/main/java/com/example/esl/Home.java
@@ -0,0 +1,25 @@
+package com.example.esl;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+    }
+
+    public void LogOut(View v){
+        Intent intent = new Intent(this, Main.class);
+        startActivity(intent);
+    }
+}
