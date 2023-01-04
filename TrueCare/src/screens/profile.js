960fe709c55946510fa89c1505ee09441bfce490
wkpescherine <wkpescherine@yahoo.com>
2023-01-03 23:31:43 -0500

    Commitand include the new screens

---
+++ TrueCare/src/screens/profile.js
@@ -0,0 +1,10 @@
+import { View, Text, Button } from "react-native"
+
+export default function Profile(){
+    return(
+        <View>
+            <Text>Profile</Text>
+            <Button></Button>
+        </View>
+    )
+}
