f26732b87c18767f54eaa438df59c8b1af856f9b
wkpescherine <wkpescherine@yahoo.com>
2023-01-03 23:27:33 -0500

    commit dashboard

---
+++ TrueCare/src/screens/dashboard.js
@@ -0,0 +1,10 @@
+import { View, Text, Button } from "react-native"
+
+export default function Dashboard(){
+    return(
+        <View>
+            <Text>Main</Text>
+            <Button></Button>
+        </View>
+    )
+}
