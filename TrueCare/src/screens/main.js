ac292275b5ccd1423ecdfe04a1bead68739903bd
wkpescherine <wkpescherine@gmail.com>
2023-01-03 23:02:00 -0500

    TrueCare - adding some screen

---
+++ TrueCare/src/screens/main.js
@@ -0,0 +1,11 @@
+import react from "react"
+import { View, Text, Button } from "react-native"
+
+export default function Main(){
+    return(
+        <View>
+            <Text>Main</Text>
+            <Button></Button>
+        </View>
+    )
+}
