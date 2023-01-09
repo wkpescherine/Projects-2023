136b9435e264c866815149cf40f867909177b93b
wkpescherine <wkpescherine@yahoo.com>
2023-01-08 23:59:57 -0500

    Updated Create Accoutn section

--- TrueCare/TrueCare/src/screens/createAccount.tsx
+++ TrueCare/TrueCare/src/screens/createAccount.tsx
@@ -1,9 +1,27 @@
-import React , { FC } from "react";
+import React , { FC, useState } from "react";
 import { View , Text , Button , TextInput} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 const App : FC = () => {
     const navigation = useNavigation();
+    const [client,setClient] = useState("Patient");
+    const [rate,setRate] = useState("Monthly");
+
+    const handleClient = () => {
+        if(client==="Patient"){
+            setClient("Medical")
+        }else{
+            setClient("Patient")
+        }
+    }
+
+    const handleRate = () => {
+        if(rate === "Monthly"){
+            setRate("Quarterly")
+        }else if (rate === "Quarterly"){
+            setRate("Semi-Annually")
+        }
+    }
 
     return(
         <View>
@@ -15,6 +33,11 @@ const App : FC = () => {
             <TextInput
                 placeholder="Enter password" 
             />
+            <Button onPress={()=>handleClient} title={client} />
+            <View>
+                <Text>Rate :</Text>
+                <Button title={rate} />
+            </View>
             <Button title="Create Account" onPress={()=>navigation.navigate("home")}/>
             <Button title="Back" onPress={()=>navigation.navigate("main")}/>
         </View>
