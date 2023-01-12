6a1f59a4abc1ced4ff987133225ace33c1119e83
wkpescherine <wkpescherine@yahoo.com>
2023-01-11 23:58:36 -0500

    Updated the Plays result section, with basic logic

--- PlayTracker/Desktop/PlayTracker/src/Sections/PlayResult.java
+++ PlayTracker/Desktop/PlayTracker/src/Sections/PlayResult.java
@@ -16,11 +16,62 @@ public class PlayResult implements ActionListener{
     
     public PlayResult(){
         completion.addActionListener(this);
+        incomplete.addActionListener(
+			new ActionListener(){
+				public void actionPerformed(ActionEvent e){
+                    Config.play_result = "INC";
+                    Display.updateUI();
+				}
+			}
+		);
+        run.addActionListener(
+			new ActionListener(){
+				public void actionPerformed(ActionEvent e){
+                    Config.play_result = "RUN";
+                    Display.updateUI();
+				}
+			}
+		);
+        interception.addActionListener(
+			new ActionListener(){
+				public void actionPerformed(ActionEvent e){
+                    Config.play_result = "INT";
+                    Display.updateUI();
+				}
+			}
+		);
+        fumble.addActionListener(
+			new ActionListener(){
+				public void actionPerformed(ActionEvent e){
+                    Config.play_result = "FUM";
+                    Display.updateUI();
+				}
+			}
+		);
+        penalty.addActionListener(
+			new ActionListener(){
+				public void actionPerformed(ActionEvent e){
+                    Config.play_result = "PEN";
+                    Display.updateUI();
+				}
+			}
+		);
+
         completion.setPreferredSize(new Dimension (39,25));
+        incomplete.setPreferredSize(new Dimension (39,25));
+        run.setPreferredSize(new Dimension (39,25));
+        interception.setPreferredSize(new Dimension (39,25));
+        fumble.setPreferredSize(new Dimension (39,25));
+        penalty.setPreferredSize(new Dimension (39,25));
 
         playresult.setPreferredSize(new Dimension (700,35));
         playresult.setLayout(new FlowLayout());
         playresult.add(completion);
+        playresult.add(incomplete);
+        playresult.add(run);
+        playresult.add(interception);
+        playresult.add(fumble);
+        playresult.add(penalty);
     }
 
     public void actionPerformed(ActionEvent e){
