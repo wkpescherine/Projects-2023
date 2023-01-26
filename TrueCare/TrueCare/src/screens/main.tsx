bb64dcbf12fe9a062b3a14f77609350fd3e0f8b2
wkpescherine <wkpescherine@yahoo.com>
2023-01-25 23:53:46 -0500

    Update Main to hide status bar

--- TrueCare/TrueCare/src/screens/main.tsx
+++ TrueCare/TrueCare/src/screens/main.tsx
@@ -1,22 +1,26 @@
 import React , { FC } from "react";
-import { View , Text, Button, StyleSheet } from "react-native";
+import { View , Text, Button } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import containers from "../Stylesheets/containers"
 import texts from "../Stylesheets/texts"
+import { StatusBar } from "expo-status-bar";
 
 const App : FC = () => {
     const navigation = useNavigation();
 
     return(
-        <View style={containers.container}>
-            <Text style={texts.basicTitle} >TrueCare</Text>
-            <View style={containers.horizontal}>
-                <Button title="Create Account" onPress={()=>navigation.navigate("create")}/>
+        <>
+        <StatusBar hidden />
+            <View style={containers.container}>
+                <Text style={texts.basicTitle}>TrueCare</Text>
+                <View style={containers.horizontal}>
+                    <Button title="Create Account" onPress={() => navigation.navigate("create")} />
+                </View>
+                <View style={containers.horizontal}>
+                    <Button title="Log-In" onPress={() => navigation.navigate("login")} />
+                </View>
             </View>
-            <View style={containers.horizontal}>
-                <Button title="Log-In" onPress={()=>navigation.navigate("login")}/>
-            </View>
-        </View>
+        </>
     )
 }
 
