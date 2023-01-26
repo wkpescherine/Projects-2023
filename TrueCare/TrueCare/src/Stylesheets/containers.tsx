501063e2cd5e3486bcd01ce339654c3838596b80
wkpescherine <wkpescherine@yahoo.com>
2023-01-25 23:13:01 -0500

    Updated basic UI Added a Text stylesheet

--- TrueCare/TrueCare/src/Stylesheets/containers.tsx
+++ TrueCare/TrueCare/src/Stylesheets/containers.tsx
@@ -5,6 +5,7 @@ export default StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: "#000000",//This gets me black
     },
     horizontal:{
         padding: 5,
