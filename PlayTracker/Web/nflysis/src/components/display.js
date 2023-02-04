45c2ea1c020e52eef66472f0f461a3b729ca96d2
wkpescherine <wkpescherine@yahoo.com>
2023-02-03 22:18:16 -0500

    Updated Display section

--- PlayTracker/Web/nflysis/src/components/display.js
+++ PlayTracker/Web/nflysis/src/components/display.js
@@ -1,14 +1,7 @@
 import React from 'react';
 import '../../src';
-import { useNavigate} from 'react-router-dom'
 
 function Display (){
-    let navigate = useNavigate();
-    
-    function RedirectTo(){
-        navigate('/')
-    }
-
     return (
         <div style={{backgroundColor:"#999999", padding: 5,}}>
             <div>
