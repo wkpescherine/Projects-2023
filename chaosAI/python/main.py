f1c2ac0141c9f8a07e64687f9c0f2f8e0f816de3
wkpescherine <wkpescherine@yahoo.com>
2023-02-13 16:22:40 -0500

    Added the ability to create the root directory

--- chaosAI/python/main.py
+++ chaosAI/python/main.py
@@ -1,11 +1,15 @@
 """
 Basic setup to try and automate site building
 """
+import os
 
 response = input("Please enter the name of the site: ")
 
 def build_site(arg):
     """This is where we will actually build the site"""
+    #This will create the file
+    os.mkdir(arg)
+    #os.chdir(arg)
     print("Site name is "+arg)
 
 class StartBuilder:
