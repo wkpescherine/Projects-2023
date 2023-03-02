6470ba515f636f269be8578f1561d4d71afebf4f
wkpescherine <wkpescherine@yahoo.com>
2023-03-01 22:01:11 -0500

    Updated Create - State and Storage for creation

--- PFS/PFS/src/screens/createAccount.tsx
+++ PFS/PFS/src/screens/createAccount.tsx
@@ -9,18 +9,20 @@ import { States } from "./components"
 const App : FC = () => {
     const navigation = useNavigation();
 
+    //LOCAL STATE
     //Log in and account related information
     const [newUserName, setNewUserName] = useState("")
     const [newPassword, setNewPassword] = useState("")
     const [confirmUsername, setConfirmUserName] = useState("")
     const [confirmPassworc, setConfirmPassword] = useState("")
 
+    //FOR LOCAL STORAGE
     //Game Data will be here actually
     const [timeRate,setTimeRate] = useState("24")
-    const [wealth, setWealth] = useState(100)
+    const [wealth, setWealth] = useState("")
     const [educationLevel, setEducationLevel] = useState("HS Dropout")
     const [employment, SetEmployment] = useState("Unemployed")
-    const [cashOnHand,setCashOnHand] = useState(100)
+    const [cashOnHand,setCashOnHand] = useState(wealth)
 
     const setRateOfTime = () => {
         if(timeRate === "24"){setTimeRate("12")}
@@ -31,28 +33,26 @@ const App : FC = () => {
     }
 
     const setEducation = () => {
-        if(educationLevel === "HS Dropout"){setEducationLevel("GED"); setWealth(500); setCashOnHand(500)}
-        else if (educationLevel === "GED"){setEducationLevel("HS Grad"); setWealth(500)}
-        else if (educationLevel === "HS Grad"){setEducationLevel("PT College");setWealth(1500)}
-        else if (educationLevel === "PT College"){setEducationLevel("FT College");setWealth(1500)}
-        else if (educationLevel === "FT College"){setEducationLevel("2yr Grad");setWealth(3000)}
-        else if (educationLevel === "2yr Grad"){setEducationLevel("4yr Grad");setWealth(3000)}
-        else if (educationLevel === "4yr Grad"){setEducationLevel("Masters"); setWealth(7500)}
-        else if (educationLevel === "Masters"){setEducationLevel("PhD"); setWealth(7500)}
-        else if (educationLevel === "PhD"){setEducationLevel("HS Dropout"); setWealth(100)}
+        if(educationLevel === "HS Dropout"){setEducationLevel("GED"); setWealth("500")}
+        else if (educationLevel === "GED"){setEducationLevel("HS Grad"); setWealth("500")}
+        else if (educationLevel === "HS Grad"){setEducationLevel("PT College");setWealth("1500")}
+        else if (educationLevel === "PT College"){setEducationLevel("FT College");setWealth("1500")}
+        else if (educationLevel === "FT College"){setEducationLevel("2yr Grad");setWealth("3000")}
+        else if (educationLevel === "2yr Grad"){setEducationLevel("4yr Grad");setWealth("3000")}
+        else if (educationLevel === "4yr Grad"){setEducationLevel("Masters"); setWealth("7500")}
+        else if (educationLevel === "Masters"){setEducationLevel("PhD"); setWealth("7500")}
+        else if (educationLevel === "PhD"){setEducationLevel("HS Dropout"); setWealth("100")}
     }
 
     const saveData = async() =>{
-        const stringWealth = wealth.toString()
-        const stringCashOnHand = cashOnHand.toString()
         try{
             await AsyncStorage.setItem('username', newUserName);
             await AsyncStorage.setItem('password', newPassword);
-            await AsyncStorage.setItem('wealth', stringWealth );
+            await AsyncStorage.setItem('wealth', wealth );
             await AsyncStorage.setItem('status', "Active" );
             await AsyncStorage.setItem('timeRate', timeRate );
             await AsyncStorage.setItem('employment', employment );
-            await AsyncStorage.setItem('cashOnHand', stringCashOnHand );
+            await AsyncStorage.setItem('cashOnHand', cashOnHand );
             await AsyncStorage.setItem('chaseAccount', "0" );
             await AsyncStorage.setItem('boaAccount', "0" );
             navigation.navigate("dashboard")
