eafb1598c099048e144eaec9e69c03122c021c87
wkpescherine <wkpescherine@yahoo.com>
2023-03-04 01:34:12 -0500

    Updated CreditCards: Added in basic setup

--- PFS/PFS/src/screens/creditCards.tsx
+++ PFS/PFS/src/screens/creditCards.tsx
@@ -1,5 +1,5 @@
-import React , { FC } from "react";
-import { View , Text, Button } from "react-native";
+import React , { FC, useState } from "react";
+import { View , Text, Button , Image} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 //import containers from "../Stylesheets/containers"
 import mainStyle from "../stylesheets/mainStyleSheet"
@@ -8,10 +8,49 @@ import { StatusBar } from "expo-status-bar";
 const App : FC = () => {
     const navigation = useNavigation();
 
+    const [visaLimit, setVisaLimit] = useState(1000)
+    const [visaInterest, setVisaInterest] = useState(2.1)
+    const [visaBalance, setVisaBalance] = useState(0)
+    const [visaAvailable, setVisaAvailable] = useState(0)
+
     return(
         <><StatusBar hidden />
             <View style={mainStyle.container}>
-                <Text style={mainStyle.basicText}>Credit Cards Section</Text>
+                <Text style={mainStyle.basicTitle}>Credit Cards</Text>
+                <View style={mainStyle.spacer} />
+                <View style={mainStyle.horizonFlow}>
+                    <Image source={require("../images/visaCC.png")} style={{width: 90, height: 60}} />
+                    <View>
+                        <View style={mainStyle.horizonFlow}>
+                            <Text style={mainStyle.basicText}>Limit: ${visaLimit}</Text>
+                            <Text style={mainStyle.basicText}>Interest: {visaInterest}%</Text>
+                        </View>
+                        <View>
+                            <View style={mainStyle.horizonFlow}>
+                                <Text style={mainStyle.basicText}>Balance: ${visaBalance}</Text>
+                                <Text style={mainStyle.basicText}>Available: S{visaAvailable}%</Text>
+                            </View>
+                        </View>
+                    </View>
+                </View>
+                <View style={mainStyle.spacer} />
+                <View style={mainStyle.horizonFlow}>
+                    <Image source={require("../images/mastercardCC.png")} style={{width: 90, height: 60}} />
+                    <View>
+                        <View style={mainStyle.horizonFlow}>
+                            <Text style={mainStyle.basicText}>Limit: ${visaLimit}</Text>
+                            <Text style={mainStyle.basicText}>Interest: {visaInterest}%</Text>
+                        </View>
+                        <View>
+                            <View style={mainStyle.horizonFlow}>
+                                <Text style={mainStyle.basicText}>Balance: ${visaBalance}</Text>
+                                <Text style={mainStyle.basicText}>Available: S{visaAvailable}%</Text>
+                            </View>
+                        </View>
+                    </View>
+                </View>
+                <View style={mainStyle.spacer} />
+
                 <Button title="Back to Dashboard" onPress={() => navigation.navigate("dashboard")}/>
             </View>
         </>
