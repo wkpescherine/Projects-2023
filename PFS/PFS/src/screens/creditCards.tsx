6905c4a2c1eeaf02037509c0f48fee4e77c4ccbd
wkpescherine <wkpescherine@yahoo.com>
2023-03-04 23:49:02 -0500

    Update Credit: Adding logic for tiered credit

--- PFS/PFS/src/screens/creditCards.tsx
+++ PFS/PFS/src/screens/creditCards.tsx
@@ -9,33 +9,46 @@ import { CCPayments } from "./components";
 const App : FC = () => {
     const navigation = useNavigation();
 
-    const [visaLimit, setVisaLimit] = useState(1000)
-    const [visaInterest, setVisaInterest] = useState(2.1)
-    const [visaBalance, setVisaBalance] = useState(0)
-    const [visaAvailable, setVisaAvailable] = useState(0)
+    const [ficoScore , steFicoScore] = useState("600")
+
+    const [visaStatus, setVisaStatus] = useState("Apply")
+    const [visaLimit, setVisaLimit] = useState("1000")
+    const [visaInterest, setVisaInterest] = useState("19.8")
+    const [visaBalance, setVisaBalance] = useState("0")
+    const [visaAvailable, setVisaAvailable] = useState("0")
 
     return(
         <><StatusBar hidden />
             <View style={mainStyle.container}>
                 <Text style={mainStyle.basicTitle}>Credit Cards</Text>
                 <View style={mainStyle.spacer} />
-                <View style={mainStyle.horizonFlow}>
-                    <Image source={require("../images/visaCC.png")} style={{width: 90, height: 60}} />
+                {visaStatus === "Apply" &&
                     <View>
-                        <View style={mainStyle.horizonFlow}>
-                            <Text style={mainStyle.basicText}>Limit: ${visaLimit}</Text>
-                            <Text style={mainStyle.basicText}>Interest: {visaInterest}%</Text>
-                        </View>
+                        <Button title="Basic Visa Apply" />
+                    </View>
+                }
+                {visaStatus ==="Basic" &&
+                    <View style={mainStyle.horizonFlow}>
+                        <Image source={require("../images/visaCC.png")} style={{width: 90, height: 60}} />
                         <View>
                             <View style={mainStyle.horizonFlow}>
-                                <Text style={mainStyle.basicText}>Balance: ${visaBalance}</Text>
-                                <Text style={mainStyle.basicText}>Available: S{visaAvailable}%</Text>
+                                <Text style={mainStyle.basicText}>Limit: ${visaLimit}</Text>
+                                <Text style={mainStyle.basicText}>Interest: {visaInterest}%</Text>
+                            </View>
+                            <View>
+                                <View style={mainStyle.horizonFlow}>
+                                    <Text style={mainStyle.basicText}>Balance: ${visaBalance}</Text>
+                                    <Text style={mainStyle.basicText}>Available: S{visaAvailable}%</Text>
+                                </View>
                             </View>
                         </View>
                     </View>
-                </View>
+                }
                 <CCPayments />
                 <View style={mainStyle.spacer} />
+                <View>
+                    <Button title="Basic Mastercard Apply" />
+                </View>
                 <View style={mainStyle.horizonFlow}>
                     <Image source={require("../images/mastercardCC.png")} style={{width: 90, height: 60}} />
                     <View>
@@ -53,7 +66,9 @@ const App : FC = () => {
                 </View>
                 <CCPayments />
                 <View style={mainStyle.spacer} />
-                <View style={mainStyle.spacer} />
+                <View>
+                    <Button title="Basic Amex Apply" />
+                </View>
                 <View style={mainStyle.horizonFlow}>
                     <Image source={require("../images/amex.png")} style={{width: 90, height: 60}} />
                     <View>
@@ -70,6 +85,10 @@ const App : FC = () => {
                     </View>
                 </View>
                 <CCPayments />
+                <View style={mainStyle.spacer} />
+                <View>
+                    <Text>Fico Score: {ficoScore}</Text>
+                </View>
                 <View style={mainStyle.spacer} />
                 <Button title="Back to Dashboard" onPress={() => navigation.navigate("dashboard")}/>
             </View>
