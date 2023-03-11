65ef546b8de97af819580b41926831b381e25758
wkpescherine <wkpescherine@yahoo.com>
2023-03-10 22:18:30 -0500

    Created Credit Card Widgets and section Pulling each card out into its own section.

--- PFS/PFS/src/screens/components/index.js
+++ PFS/PFS/src/screens/components/index.js
@@ -1,9 +1,13 @@
+//Same folder and 
+import States from "./statesWidget"
+//All widgets for the Dashboard Screen
 import Stocks from "./DashBoardWidgets/stockMarketWidget";
 import Bank from "./DashBoardWidgets/bankWidget"
 import Occupation from "./DashBoardWidgets/occupationWidget"
 import CreditCards from "./DashBoardWidgets/creditCardWidget"
-import States from "./statesWidget"
 import DailyTasks from "./DashBoardWidgets/dailyTaskWidget"
+//All widgets for the Credit Card Screen
 import CCPayments from "./CreditCardWidgets/ccPaymentOptions"
+import VisaCard from "./CreditCardWidgets/visaCardWidget"
 
-export { Stocks , Bank , Occupation , CreditCards , States , DailyTasks , CCPayments};
+export { Stocks , Bank , Occupation , CreditCards , States , DailyTasks , CCPayments , VisaCard};
--- PFS/PFS/src/screens/creditCards.tsx
+++ PFS/PFS/src/screens/creditCards.tsx
@@ -3,19 +3,13 @@ import { View , Text, Button , Image} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import mainStyle from "../stylesheets/mainStyleSheet"
 import { StatusBar } from "expo-status-bar";
-import { CCPayments } from "./components";
+import { CCPayments, VisaCard} from "./components";
 
 const App : FC = () => {
     const navigation = useNavigation();
 
     const [ficoScore , steFicoScore] = useState(600)
 
-    //VISA DATA
-    const [visaStatus, setVisaStatus] = useState("Apply")
-    const [visaLimit, setVisaLimit] = useState("1000")
-    const [visaInterest, setVisaInterest] = useState("19.8")
-    const [visaBalance, setVisaBalance] = useState("0")
-    const [visaAvailable, setVisaAvailable] = useState("0")
     //MASTERCARD DATA
     const [mastercardStatus, setMastercardStatus] = useState("Apply")
     const [mastercardLimit, setMastercardLimit] = useState("1000")
@@ -42,39 +36,7 @@ const App : FC = () => {
         <><StatusBar hidden />
             <View style={mainStyle.container}>
                 <Text style={mainStyle.basicTitle}>Credit Cards</Text>
-                <View style={mainStyle.spacer} />
-                {/* This section is for the Visa CC */}
-                {visaStatus === "Apply" &&
-                    <View>
-                        <Button title="Basic Visa Apply" onPress={() => upgradeCC("visa")}/>
-                    </View>
-                }
-                {visaStatus ==="Basic" &&
-                    <View style={mainStyle.horizonFlow}>
-                        <Image source={require("../images/visaCC.png")} style={{width: 90, height: 60}} />
-                        <View>
-                            <View style={mainStyle.horizonFlow}>
-                                <Text style={mainStyle.basicText}>Limit: ${visaLimit}</Text>
-                                <Text style={mainStyle.basicText}>Interest: {visaInterest}%</Text>
-                            </View>
-                            <View>
-                                <View style={mainStyle.horizonFlow}>
-                                    <Text style={mainStyle.basicText}>Balance: ${visaBalance}</Text>
-                                    <Text style={mainStyle.basicText}>Available: S{visaAvailable}%</Text>
-                                </View>
-                            </View>
-                        </View>
-                        <Button title="Apply to upgrade to Platnium" />
-                    </View>
-                }
-                {visaStatus !== "Apply" &&
-                    <CCPayments />
-                }
-                {visaStatus === "Basic" &&
-                    <View>
-                        <Button title="Apply to Upgrade to Platnium" onPress={() => upgradeCC("visa")}/>
-                    </View>
-                }
+                <VisaCard />
                 <View style={mainStyle.spacer} />
                 {/* This section is for the Mastercard CC */}
                 {mastercardStatus === "Apply" &&
