d95ecb4718a5fd701064bc240733d6012ffe7a23
wkpescherine <wkpescherine@yahoo.com>
2023-03-10 22:01:49 -0500

    Update to widget folders

--- PFS/PFS/src/screens/components/index.js
+++ PFS/PFS/src/screens/components/index.js
@@ -1,9 +1,9 @@
 import Stocks from "./stockMarketWidget";
-import Bank from "./bankWidget"
+import Bank from "./DashBoardWidgets/bankWidget"
 import Occupation from "./occupationWidget"
 import CreditCards from "./creditCardWidget"
 import States from "./statesWidget"
 import DailyTasks from "./dailyTaskWidget"
-import CCPayments from "./ccPaymentOptions"
+import CCPayments from "./CreditCardWidgets/ccPaymentOptions"
 
 export { Stocks , Bank , Occupation , CreditCards , States , DailyTasks , CCPayments};
