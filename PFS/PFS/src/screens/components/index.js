e5509f6a38f684331141516da899b8f48aecef46
wkpescherine <wkpescherine@yahoo.com>
2023-03-11 23:27:00 -0500

    Updated file structure

--- PFS/PFS/src/screens/components/index.js
+++ PFS/PFS/src/screens/components/index.js
@@ -2,12 +2,12 @@
 import States from "./statesWidget"
 //All widgets for the Dashboard Screen
 import Stocks from "./DashBoardWidgets/stockMarketWidget";
-import Bank from "./DashBoardWidgets/bankWidget"
+import Bank from "./bankWidget"
 import Occupation from "./DashBoardWidgets/occupationWidget"
 import CreditCards from "./DashBoardWidgets/creditCardWidget"
 import DailyTasks from "./DashBoardWidgets/dailyTaskWidget"
 //All widgets for the Credit Card Screen
-import CCPayments from "./CreditCardWidgets/ccPaymentOptions"
+import CCPayments from "./ccPaymentOptions"
 import VisaCard from "./CreditCardWidgets/visaCardWidget"
 
 export { Stocks , Bank , Occupation , CreditCards , States , DailyTasks , CCPayments , VisaCard};
