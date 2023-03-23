cd24294fa2b9709cfb1d0dae85808e1473e295e3
wkpescherine <wkpescherine@yahoo.com>
2023-03-23 00:03:28 -0400

    Updated Main Activity Added Selection Actitivty and some basic layout

--- Prime/app/src/main/java/com/example/prime/Selection.java
+++ Prime/app/src/main/java/com/example/prime/Selection.java
@@ -7,19 +7,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class Selection extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_selection);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
     }
 
-    public void enter(View v){
-        Intent intent = new Intent(this, Selection.class);
+    public void exit(View v){
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
