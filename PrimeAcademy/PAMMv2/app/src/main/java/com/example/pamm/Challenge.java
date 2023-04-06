70881f7b02d9bc25dda93281557771ddb727bd9e
wkpescherine <wkpescherine@yahoo.com>
2023-04-05 23:12:36 -0400

    Added the challenge section Added keypad Added some basic logic

--- PrimeAcademy/PAMMv2/app/src/main/java/com/example/pamm/Challenge.java
+++ PrimeAcademy/PAMMv2/app/src/main/java/com/example/pamm/Challenge.java
@@ -2,13 +2,40 @@ package com.example.pamm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.TextView;
 
 public class Challenge extends AppCompatActivity {
+    String answer = "";
+    String question = "";
+    Integer solution = 0;
+    Integer answerInt = 0;
+    Double answerDecimal = 0.000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_challenge);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+    }
+
+    public void handleKeypadNum(View v){
+        TextView t = (TextView) v;
+        String valueSelect = t.getText().toString();
+        answer += valueSelect;
+        //data.currentNum += valueSelect;
+        //renderSection();
+        TextView answerText = findViewById(R.id.answer);
+        answerText.setText(answer);
+    }
+
+    public void backToDashboard(View v){
+        Intent intent = new Intent(this, Dashboard.class);
+        startActivity(intent);
     }
 }
