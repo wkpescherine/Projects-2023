6b40068e1a0978ab273712855fb8b66b3472b4b1
wkpescherine <wkpescherine@yahoo.com>
2023-04-21 22:46:00 -0400

    Update Q20Challenge wityh proper logic

--- PrimeAcademy/PAMMv2/app/src/main/java/com/example/pamm/ChallengeQ20.java
+++ PrimeAcademy/PAMMv2/app/src/main/java/com/example/pamm/ChallengeQ20.java
@@ -5,8 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
-import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class ChallengeQ20 extends AppCompatActivity {
     Data data = new Data();
@@ -42,16 +43,54 @@ public class ChallengeQ20 extends AppCompatActivity {
         answerText.setText(answer);
     }
 
+    public void q20GameLogic (){
+        TextView num1 = findViewById(R.id.number1);
+        TextView num2 = findViewById(R.id.number2);
+        TextView sym = findViewById(R.id.symbol);
+        Random rnd = new Random();
+        Integer symbolValue = rnd.nextInt(data.symbolBound);
+        String symbolUsed = symbolArray[symbolValue];
+        int boundValue = 0;
+        if(symbolValue == 0){ boundValue = data.addBoundValue;}
+        if(symbolValue == 1){ boundValue = data.subBoundValue;}
+        if(symbolValue == 2){ boundValue = data.multiBoundValue;}
+        if(symbolValue == 3){ boundValue = data.divBoundValue;}
+        Integer rndNum1 = rnd.nextInt(boundValue)+1;
+        Integer rndNum2 = rnd.nextInt(boundValue)+1;
+        solution = solve.basicFormulas(symbolUsed, rndNum1, rndNum2);
+        sym.setText(symbolUsed);
+        num1.setText(rndNum1.toString());
+        num2.setText(rndNum2.toString());
+    }
+
+    public void checkSolution(View v){
+        data.totalAsked += 1;
+        TextView response = findViewById(R.id.cresponse);
+        if(answer.equals("")){
+            response.setText("No answer");
+            data.q20Asked +=1;
+        } else if(solution == Double.valueOf(answer)){
+            response.setText("Correct");
+            data.totalCorrect += 1;
+            data.q20Asked += 1;
+            data.q20Solved +=1;
+        } else {
+            response.setText("Incorrect");
+            data.q20Asked +=1;
+        }
+        setDataUI();
+    }
+
     public void setDataUI(){
-        LinearLayout q20Head = findViewById(R.id.q20Header);
-        LinearLayout standardHead = findViewById(R.id.standardHeader);
-        TextView dataGrade = findViewById(R.id.cgrade);
-        TextView dataTier = findViewById(R.id.ctier);
-        TextView answersSolved = findViewById(R.id.csolve);
         TextView dataQ20Asked = findViewById(R.id.q20Questions);
         TextView dataq20Correct = findViewById(R.id.q20QuestionsCorrect);
         TextView answerString = findViewById(R.id.answer);
-        data.checkNextTier();
+        dataQ20Asked.setText(data.q20Asked+" of 20");
+        dataq20Correct.setText("Correct :"+ data.q20Solved);
+        answerString.setText("");
+        answerString.setText("");
+        answer="";
+        q20GameLogic();
 
     }
 }
