778d861f2f68093b423efe98841e48eab2dffa00
wkpescherine <wkpescherine@yahoo.com>
2023-05-28 01:22:48 -0400

    Version 0.33.3 Removed used dashboard imports Fixed minor Practice display issues

--- PrimeAcademy/PAMMv2/app/src/main/java/com/example/pamm/Dashboard.java
+++ PrimeAcademy/PAMMv2/app/src/main/java/com/example/pamm/Dashboard.java
@@ -9,15 +9,9 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.TextView;
 import android.widget.Toast;
-import com.google.android.gms.ads.MobileAds;
-import com.google.android.gms.ads.initialization.InitializationStatus;
-import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
 
 public class Dashboard extends AppCompatActivity {
     Data data = new Data();
-    //private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- PrimeAcademy/PAMMv2/app/src/main/java/com/example/pamm/Practice.java
+++ PrimeAcademy/PAMMv2/app/src/main/java/com/example/pamm/Practice.java
@@ -44,23 +44,33 @@ public class Practice extends AppCompatActivity {
     }
 
     public void practiceGameLogic(){
+        TextView pracFormula = findViewById(R.id.pracFormulaString);
         TextView pracTierValue = findViewById(R.id.practiceTier);
         pracTierValue.setText("Tier: " +Data.practiceTier);
         pracData.checkNextTier();
         pracFormulaBuilder.builder();
+        pracFormula.setText(Data.formulaString);
     }
     public void checkPracSolution(View v){
         TextView response = findViewById(R.id.cresponse);
-        if(answer.equals(Data.solution)){
+        TextView pAnswer = findViewById(R.id.answer);
+        if(Data.solution == Double.valueOf(answer)){
             response.setText("Correct");
         } else {
             response.setText("Incorrect! The answer is " + Data.solution);
         }
+        answer="";
+        pAnswer.setText("");
+        practiceGameLogic();
     }
 
     public void decreaseTier(View v){
-        if(Data.practiceTier > 1){Data.practiceTier -= 1;}
-        practiceGameLogic();
+        if(Data.practiceTier > 1){
+            Data.practiceTier -= 1;
+            practiceGameLogic();
+        }else {
+            Toast.makeText(getApplicationContext(),"Your are at the lowest possible Tier", Toast.LENGTH_SHORT).show();
+        }
     }
 
     public void increaseTier(View v){
@@ -68,10 +78,11 @@ public class Practice extends AppCompatActivity {
             Toast.makeText(getApplicationContext(),"Current Tier is 0 unable to raise it", Toast.LENGTH_SHORT).show();
         } else if(Data.practiceTier < Data.highestTier){
             Data.practiceTier +=1;
+            practiceGameLogic();
+
         } else {
             Toast.makeText(getApplicationContext(),"Your Practice Tier is at the highest possible", Toast.LENGTH_SHORT).show();
         }
-        practiceGameLogic();
     }
 
     public void backToDashboard(View v){
