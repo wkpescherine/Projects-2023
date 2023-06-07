4bb5d86c6f07dc906e751e939987d4b31fc62a0d
wkpescherine <wkpescherine@yahoo.com>
2023-06-06 20:41:07 -0400

    Ascension - C# conversion

---
+++ Ascension/Ascensionv1/App.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace Test
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Ascension/Ascensionv1/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace Test
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
