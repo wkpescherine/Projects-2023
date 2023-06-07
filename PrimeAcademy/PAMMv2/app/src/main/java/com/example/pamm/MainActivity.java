8b11f6c662bb4adb21e96adb1a8aab5ee87c4d9b
wkpescherine <wkpescherine@yahoo.com>
2023-06-06 22:27:55 -0400

    Version0.34.1 General: Added Version checking to the system Challenge: Simplied some questions dealing with minus and division

--- PrimeAcademy/PAMMv2/app/src/main/java/com/example/pamm/MainActivity.java
+++ PrimeAcademy/PAMMv2/app/src/main/java/com/example/pamm/MainActivity.java
@@ -40,15 +40,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void clickToStart(View v){
+        checkVersion();
         Intent intent = new Intent(this, Dashboard.class);
         startActivity(intent);
     }
 
     public void checkVersion(){
-        SharedPreferences sp = getSharedPreferences("PAMM", MODE_PRIVATE);
-        int currentVersion = sp.getInt("version", 0);
+        SharedPreferences sp2 = getSharedPreferences("PAMM",MODE_PRIVATE);
+        int currentVersion;
+        currentVersion = sp2.getInt("version", 1);
+        Toast.makeText(getApplicationContext(),"outside", Toast.LENGTH_SHORT).show();
         if(currentVersion == 340){
             Toast.makeText(getApplicationContext(),"Version up to Date", Toast.LENGTH_SHORT).show();
+        }else{
+            SharedPreferences.Editor editor2 = sp2.edit();
+            editor2.putInt("version", 340);
+            editor2.commit();
+            Toast.makeText(getApplicationContext(),"In else "+currentVersion, Toast.LENGTH_SHORT).show();
         }
     }
 }
