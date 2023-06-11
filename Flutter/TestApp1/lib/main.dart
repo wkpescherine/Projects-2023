9e4e4e7145c4844c23b65393f4b2c2bfa814322e
wkpescherine <wkpescherine@yahoo.com>
2023-06-10 23:33:35 -0400

    Flutter update

--- Flutter/TestApp1/lib/main.dart
+++ Flutter/TestApp1/lib/main.dart
@@ -3,10 +3,18 @@ import 'package:flutter/material.dart';
 //This is required for every app as it is the starting point
 void main() {
   //Must always have the run app and the material widget as this is how we build
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     //The first widget must be home
-    home: Center(
-      child: Text("Hello"),
+    //home: Center(
+    //  child: Text("Hello"),
+    //),
+    home: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey[800],
+      ),
+      body: const Center(
+        child: Text("Test Here"),
+      ),
     ),
   ));
 }
