e2d0cbbc964c37ff04db94394c822dd46eadd463
wkpescherine <wkpescherine@yahoo.com>
2023-06-15 01:17:53 -0400

    Ironman version 2 Dashboard: Fixed image sizing NBA: Added list creation of game types NBA: Added 2 games, cpu and icpu

--- IronmanEFSL/app/src/main/java/com/example/ironman/NBAList.java
+++ IronmanEFSL/app/src/main/java/com/example/ironman/NBAList.java
@@ -7,19 +7,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 
-public class Dashboard extends AppCompatActivity {
+public class NBAList extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        setContentView(R.layout.activity_nbalist);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
     }
 
-    public void nbaList(View v){
-        Intent intent = new Intent(this, NBAList.class);
+    public void backToDashboard(View v){
+        Intent intent = new Intent(this, Dashboard.class);
         startActivity(intent);
     }
 }
