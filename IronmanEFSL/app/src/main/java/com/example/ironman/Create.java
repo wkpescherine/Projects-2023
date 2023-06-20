76db7d68acea9f778af41b328e00c9f047b81681
wkpescherine <wkpescherine@yahoo.com>
2023-06-20 01:05:12 -0400

    Ironman v3.1 General: Updating UI Main: Added Local storage Create: Adjusting account creation

--- IronmanEFSL/app/src/main/java/com/example/ironman/Create.java
+++ IronmanEFSL/app/src/main/java/com/example/ironman/Create.java
@@ -2,7 +2,11 @@ package com.example.ironman;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.EditText;
 
 public class Create extends AppCompatActivity {
 
@@ -10,5 +14,21 @@ public class Create extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+    }
+
+    public void onCreate(View v){
+        EditText username = findViewById(R.id.createUser);
+        EditText password = findViewById(R.id.createPass);
+        EditText email = findViewById(R.id.createEmail);
+        Intent intent = new Intent(this, Dashboard.class);
+        startActivity(intent);
+    }
+
+    public void backToMain(View v){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- IronmanEFSL/app/src/main/java/com/example/ironman/MainActivity.java
+++ IronmanEFSL/app/src/main/java/com/example/ironman/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.ironman;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
@@ -16,7 +17,25 @@ public class MainActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
+        checkPref();
     }
+
+    public void checkPref(){
+        String filename = "IRONMAN";
+        SharedPreferences sp = getSharedPreferences(filename,MODE_PRIVATE);
+        if(filename.isEmpty()){
+            SharedPreferences.Editor editor = sp.edit();
+            editor.putString("Account", "NONE");
+            editor.putString("Username", "NONE");
+            editor.putString("Password", "NONE");
+            editor.putString("Email", "NONE");
+            editor.putInt("Silver", 0);
+            editor.putInt("Gold", 0);
+            editor.putInt("Platnium", 1);
+            editor.commit();
+        }
+    }
+
     public void Login(View v){
         Intent intent = new Intent(this, Dashboard.class);
         startActivity(intent);
