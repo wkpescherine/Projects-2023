972392610b3c92bfee4258cc6b8e3c75585b8ca5
wkpescherine <wkpescherine@yahoo.com>
2023-09-04 23:53:49 -0400

    Unchained - v0.2 - Setup basic UI

--- Unchained/v0/UNCHAINED.java
+++ Unchained/v0/UNCHAINED.java
@@ -1,11 +1,19 @@
 package Unchained.v0;
 
 import javax.swing.JFrame;
+import java.awt.*;
 
 public class UNCHAINED {
     JFrame window = new JFrame("Unchained v0.1");
 
-    UNCHAINED(){}
+    UNCHAINED(){
+        window.setLayout(new FlowLayout());
+	    window.setSize(800, 600);
+		window.getContentPane().setBackground(Color.BLACK);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setVisible(true);
+		window.setResizable(false);
+    }
 
     public static void main(String arp[]){
         UNCHAINED unchained = new UNCHAINED();
