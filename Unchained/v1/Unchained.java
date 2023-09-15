a0657aed8261d8a0f18e68755588994a8158430a
wkpescherine <wkpescherine@yahoo.com>
2023-09-15 15:20:11 -0400

    Unchained - v1.2.2 -Working to upgrade game

--- Unchained/v1/Unchained.java
+++ Unchained/v1/Unchained.java
@@ -1,11 +1,21 @@
 import java.awt.*;
-import java.awt.event.*;
+//import java.awt.event.*;
 import javax.swing.*;
 
 public class Unchained {
-    JFrame window = new JFrame("Unchained v1.1.1");
+    StartScreen start = new StartScreen();
+
+    JFrame window = new JFrame("Unchained v1.2.2");
+    JPanel container = new JPanel();
 
     Unchained() {
+        container.setPreferredSize(new Dimension(880, 680));
+        container.setBounds(-40, -40, 880, 680);
+        container.setBackground(Color.RED);
+
+        container.add(start.entry);
+
+        window.add(container);
         window.setLayout(null);
         window.setSize(800, 600);
         window.getContentPane().setBackground(Color.BLACK);
