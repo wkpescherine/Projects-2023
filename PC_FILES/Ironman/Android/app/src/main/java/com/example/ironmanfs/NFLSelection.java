5dabe44ac40a009c44b45bc869f749955d15be10
William Pescherine <wkpescherine@gmail.com>
2023-09-26 00:33:58 -0400

    PC_Files -IronmanFS --Android' ---v 0.2 + Added images for NBA and NFL logos = Added Selection screen for NFL games + Added in username and password sections for login logic + Fixed UI for Main Screen + Fixed UI fro Dashboard

--- PC_FILES/Ironman/Android/app/src/main/java/com/example/ironmanfs/NFLSelection.java
+++ PC_FILES/Ironman/Android/app/src/main/java/com/example/ironmanfs/NFLSelection.java
@@ -2,28 +2,17 @@ package com.example.ironmanfs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.WindowManager;
 
-public class Dashboard extends AppCompatActivity {
+public class NFLSelection extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        setContentView(R.layout.activity_nflselection);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().hide();
-    }
-
-    public void clickExit(){
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-    }
-
-    public void clickLeague(){
-        Intent intent = new Intent(this, NFLSelection.class);
-        startActivity(intent);
     }
 }
