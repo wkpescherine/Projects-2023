7a98cd5c779c15b6af36b8f01baaced6dadb1fb7
William Pescherine <wkpescherine@gmail.com>
2023-10-11 00:19:46 -0400

    PAMM v0.43.0 -Fully implemented About section -Fully implemented Notes section -Updated UI for About section -Updated UI for Notes section

--- PC_FILES/PAMM/v0/app/src/main/java/com/example/pamm/Notes.java
+++ PC_FILES/PAMM/v0/app/src/main/java/com/example/pamm/Notes.java
@@ -2,7 +2,10 @@ package com.example.pamm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
 
 public class Notes extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class Notes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+    }
+
+    public void backToDashboard(View v){
+        Intent intent = new Intent(this, Dashboard.class);
+        startActivity(intent);
     }
 }
